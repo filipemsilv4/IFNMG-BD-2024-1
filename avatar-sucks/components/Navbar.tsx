71123803f1572d662efec59fc14c4ff483dfef60
filipemsilv4 <filipemsilva@outlook.com.br>
2024-09-20 15:57:10 -0300

    fix: mobile menu now shows all pages

--- avatar-sucks/components/Navbar.tsx
+++ avatar-sucks/components/Navbar.tsx
@@ -16,13 +16,13 @@ export function Navbar() {
             AvatarSucks
           </Link>
           <div className="hidden md:flex space-x-4">
-            <NavLink href="/insere_colonia">CriaColonia</NavLink>
-            <NavLink href="/edita_colonia">EditaColonia</NavLink>
-            <NavLink href="/explorador">Explorador</NavLink>
-            <NavLink href="/insere_arvore">CriaArvore</NavLink>
-            <NavLink href="/remove_arvore">RemoveArvore</NavLink>
-            <NavLink href="/edita_arvore">EditaArvore</NavLink>
-            <NavLink href="/relatorio">Relatorio</NavLink>
+            <NavLink href="/insere_colonia">[Criar Colonia]</NavLink>
+            <NavLink href="/edita_colonia">[Editar Colonia]</NavLink>
+            <NavLink href="/explorador">[Explorar Tabelas]</NavLink>
+            <NavLink href="/insere_arvore">[Criar Árvore]</NavLink>
+            <NavLink href="/remove_arvore">[Remover Árvore]</NavLink>
+            <NavLink href="/edita_arvore">[Editar Árvore]</NavLink>
+            <NavLink href="/relatorio">[Relatório]</NavLink>
           </div>
           <Button
             variant="ghost"
@@ -35,8 +35,13 @@ export function Navbar() {
         </div>
         {isOpen && (
           <div className="mt-4 space-y-2 md:hidden">
-            <NavLink href="/colonia" mobile>Colonia</NavLink>
-            <NavLink href="/explorador" mobile>Explorador</NavLink>
+            <NavLink href="/insere_colonia" mobile>[Criar Colonia]</NavLink>
+            <NavLink href="/edita_colonia" mobile>[Editar Colonia]</NavLink>
+            <NavLink href="/explorador" mobile>[Explorar Tabelas]</NavLink>
+            <NavLink href="/insere_arvore" mobile>[Criar Árvore]</NavLink>
+            <NavLink href="/remove_arvore" mobile>[Remover Árvore]</NavLink>
+            <NavLink href="/edita_arvore" mobile>[Editar Árvore]</NavLink>
+            <NavLink href="/relatorio" mobile>[Relatório]</NavLink>
           </div>
         )}
       </div>
